6e8f704a4d1a6e415b940dc61e471ec9bdaaa41a
Winston Kinoti <prog.kinoti@gmail.com>
2023-02-09 15:42:13 +0300

    feat(colorscheme): use duskfox theme

---
+++ lua/plugins/duskfox.lua
@@ -0,0 +1,12 @@
+return {
+  -- add duskfox
+  { "EdenEast/nightfox.nvim" },
+
+  -- Configure LazyVim to load duskfox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "duskfox",
+    },
+  },
+}
