4ff35bf463a787a5e680ffc9195e8c98136974a8
Winston Kinoti <prog.kinoti@gmail.com>
2023-02-10 13:31:52 +0300

    fix(options): don't create neovim swap files

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local opt = vim.opt
+
+opt.backup = false -- creates a backup file
+opt.guifont = "monospace:h17" -- the font used in graphical neovim applications
+opt.scrolloff = 8 -- Lines of context
+opt.swapfile = false -- creates a swapfile
+opt.whichwrap:append "<,>,[,],h,l" -- keys allowed to move to the previous/next line when the beginning/end of line is reached
+opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
