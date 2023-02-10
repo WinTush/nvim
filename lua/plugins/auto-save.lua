697730d5b79c0f3ae56033be412a02b87591d172
Winston Kinoti <prog.kinoti@gmail.com>
2023-02-10 13:03:07 +0300

    fix(auto-save): better autoformat on saving

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -5,10 +5,22 @@ return {
     callbacks = {
       before_saving = function()
         local mode = vim.api.nvim_get_mode()
-        if mode.mode == "i" then -- Don't save while we in insert mode (triggered with autopair and such)
+        if mode.mode == "i" or mode.mode == "v" then -- Don't save while we in insert mode (triggered with autopair and such)
           vim.g.auto_save_abort = true
         else
-          vim.lsp.buf.format({ async = true })
+          local buf = vim.api.nvim_get_current_buf()
+          local ft = vim.bo[buf].filetype
+          local have_nls = #require("null-ls.sources").get_available(ft, "NULL_LS_FORMATTING") > 0
+
+          vim.lsp.buf.format(vim.tbl_deep_extend("force", {
+            bufnr = buf,
+            filter = function(client)
+              if have_nls then
+                return client.name == "null-ls"
+              end
+              return client.name ~= "null-ls"
+            end,
+          }, require("lazyvim.util").opts("nvim-lspconfig").format or {}))
         end
       end,
     },
