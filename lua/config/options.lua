de04420bd2ca3fac86b4fe4c38f648d45ae50cb0
Winston Kinoti <prog.kinoti@gmail.com>
2023-07-31 21:54:00 +0300

    feat(config): update options.lua
      - Update the `opt.formatoptions` value to include "jqlnt"
      - Update the `opt.whichwrap` value to append "<,>,[,],h,l"

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,8 +4,9 @@
 local opt = vim.opt
 
 opt.backup = false -- creates a backup file
+opt.formatoptions = "jqlnt"
 opt.guifont = "monospace:h17" -- the font used in graphical neovim applications
 opt.scrolloff = 8 -- Lines of context
 opt.swapfile = false -- creates a swapfile
-opt.whichwrap:append "<,>,[,],h,l" -- keys allowed to move to the previous/next line when the beginning/end of line is reached
+opt.whichwrap:append("<,>,[,],h,l") -- keys allowed to move to the previous/next line when the beginning/end of line is reached
 opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
