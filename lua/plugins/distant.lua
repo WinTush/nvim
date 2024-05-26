660160cf94420f1a6d443d3558d248a53c5615f2
Winston Kinoti <prog.kinoti@gmail.com>
2024-05-26 13:01:00 +0300

    fix(distant): setup plugin using require("distant"):setup

--- lua/plugins/distant.lua
+++ lua/plugins/distant.lua
@@ -1,6 +1,9 @@
 return {
   {
     "chipsenkbeil/distant.nvim",
+    config = function()
+      require("distant"):setup()
+    end,
     opts = function()
       local status_ok, distant_settings = pcall(require, "distant.settings")
       if not status_ok then
