4ef09ee415cc8e676303b45a14e70469dbef53d6
Winston Kinoti <prog.kinoti@gmail.com>
2024-05-26 12:40:45 +0300

    fix(dashboard): replace alpha-nvim with dashboard-nvim
      - Removed alpha-nvim configuration from `alpha.lua`
      - Added dashboard-nvim configuration in `dashboard.lua`
      - Maintained the same keybinding for dashboard (<leader>;)

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,24 @@
+return {
+  "nvimdev/dashboard-nvim",
+  keys = {
+    {
+      "<leader>;",
+      "<cmd>Dashboard<cr>",
+      desc = "Dashboard",
+    },
+  },
+  opts = function(_, opts)
+    local logo = [[
+           ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
+           ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z    
+           ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z       
+           ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z         
+           ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║           
+           ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝           
+                                [ @kinotiwinston ]                            
+      ]]
+
+    logo = string.rep("\n", 8) .. logo .. "\n\n"
+    opts.config.header = vim.split(logo, "\n", { trimempty = true })
+  end,
+}
